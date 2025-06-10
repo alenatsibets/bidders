56ddbb2c9638f4c0212411f80bb63de244b43423
alenatsibets <128285549+alenatsibets@users.noreply.github.com>
2025-06-10 19:41:59 +0300

    Update session.py

--- app/db/session.py
+++ app/db/session.py
@@ -3,11 +3,17 @@ from sqlalchemy.orm import sessionmaker, declarative_base
 from contextlib import asynccontextmanager
 import os
 from dotenv import load_dotenv
+import ssl
 
 load_dotenv()
 DATABASE_URL = os.getenv("DB_URL")
 
-engine = create_async_engine(DATABASE_URL, echo=True, connect_args={"ssl": True})
+ssl_context = ssl.create_default_context()
+connect_args = {"ssl": ssl_context}
+
+engine = create_async_engine(
+    DATABASE_URL, echo=True, connect_args=connect_args
+)
 async_session = sessionmaker(engine, class_=AsyncSession, expire_on_commit=False)
 Base = declarative_base()
 
